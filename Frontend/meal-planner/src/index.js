5e5267bcb8d95cb2e4f06ab3499695200cd0ddf4
raculber <rculbert5678@gmail.com>
2021-06-29 14:26:04 -0500

    more code

--- Frontend/meal-planner/src/index.js
+++ Frontend/meal-planner/src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux;'
-
+import { Provider } from 'react-redux';
+import store from "./store/store";
 ReactDOM.render(
   <Provider store={store}>
   <BrowserRouter>
